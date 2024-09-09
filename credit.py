85c0bd1887d389f35727d4ad506129909acb1d7e
JL336699 <joseph.lapsley@abrdn.com>
2024-09-09 12:35:43 -0400

    huh

--- credit.py
+++ credit.py
@@ -7,7 +7,7 @@ def get_bloomberg_data(cusip):
     """Retrieve Bloomberg data for the given CUSIP."""
     try:
         # Requesting data for the given CUSIP
-        data = blp.bdp(cusip, fields=[
+        fields = [
             'SALES_REV_TURN', 'IS_TOT_OPER_EXP', 'IS_DEPR_EXP', 'IS_INT_EXPENSE',
             'EBITDA', 'EBITDA_TO_REVENUE', 'EBIDA', 'EBIDA_MARGIN',
             'OPERATING_EBIDA', 'OPERATING_EBIDA_MARGIN', 'ARDR_Annual_debt_service',
@@ -15,7 +15,9 @@ def get_bloomberg_data(cusip):
             'BS_ANN_DEBT_SERVICE', 'CASH_AND_MARKETABLE_SECURITIES',
             'BS_CASH_NEAR_CASH_ITEM', 'BS_TOTAL_DEBT_OUTSTANDING', 'BS_LT_BORROW',
             'BS_PENSIONS_LT_LIABS', 'BS_OPRB_LT_LIABS'
-        ])
+        ]
+        data = blp.bdp(cusip, flds=fields)
+        
         if data.empty:
             st.write("No data found for the provided CUSIP.")
         return data
@@ -27,7 +29,7 @@ def get_comparables(cusip):
     """Retrieve comparable CUSIPs in the same sector."""
     try:
         # Retrieve the sector code for the given CUSIP
-        sector_code = blp.bdp(cusip, fields=['SECTOR'])
+        sector_code = blp.bdp(cusip, flds=['SECTOR'])
         sector_code = sector_code['SECTOR'].values[0] if not sector_code.empty else None
         
         if not sector_code:
@@ -35,7 +37,7 @@ def get_comparables(cusip):
             return pd.DataFrame()
         
         # Request comparable data based on sector
-        comparables = blp.bds(f"Sector:{sector_code}", fields=[
+        comparables = blp.bds(f"Sector:{sector_code}", flds=[
             'SECTOR', 'NAME', 'LAST_PRICE'
         ])
         return comparables
